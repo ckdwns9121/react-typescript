4db5ce695ef74283307213e3cbdfa60f959ec006
ckdwns9121 <ckdwns9121@naver.com>
2021-05-10 15:23:43 +0900

    # 리액트 타입스크립트 튜토리얼

--- src/container/AboutContainer.tsx
+++ src/container/AboutContainer.tsx
@@ -1,7 +1,11 @@
-function AboutContainer (){
+type AboutProps ={
+    id: string
+}
+
+function AboutContainer ({id}:AboutProps){
     return(
         <div>
-            어바웃
+            어바웃 {id}
         </div>
     )
 }
--- src/page/About.tsx
+++ src/page/About.tsx
@@ -1,9 +1,21 @@
 import {RouteComponentProps} from 'react-router-dom';
 import AboutContainer from '../container/AboutContainer';
-function About({match}:RouteComponentProps){
+
+
+interface MatchParams {
+    id: string
+}
+
+/* 
+    타입스크립트에서 match값을 쓰려면 RouteComponentProps 타입이 필요
+    하지만 params에 있는 id를 꺼내 쓰려면 RouteComponentProps에서 제네릭에 타입을 주지않았다
+
+*/
+function About({match}:RouteComponentProps<MatchParams>){
     console.log(match);
+    const {id} = match.params;
     return(
-        <AboutContainer/>
+        <AboutContainer id={id}/>
     )
 }
 
